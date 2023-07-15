9ec1f4a842319610799ed0cdbdea5215edc5eb53
soheil <soheilrasekh555@gmail.com>
2023-07-15 17:59:14 +0330

    add .env.example

--- src/components/orderConfirmed.tsx
+++ src/components/orderConfirmed.tsx
@@ -14,7 +14,7 @@ export function OrderConfirmed() {
   useEffect(() => {
     emptyCart()
     setPaymentIntentId('')
-  }, [emptyCart])
+  }, [emptyCart, setPaymentIntentId])
 
   return (
     <motion.div
